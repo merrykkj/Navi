a3638520fd92f5ca17e5ef505a8cc81ae901ce7a
merrykkj <vlucena170@gmail.com>
2025-11-17 09:01:40 -0300

    iniciando página de histórico

--- mobile/navi/navigation.js
+++ mobile/navi/navigation.js
@@ -5,10 +5,10 @@ import { LoginForm } from "./src/app/account/index.js";
 import { ForgotPassword } from "./src/app/account/forgot-password/forgot-password.js";
 import Account from "./src/app/account/account.js";
 
-import Main from "./src/app/main/main.js";
-import History from "./src/app/history/history.js";
-import Settings from "./src/app/settings/settings.js";
-import Help from "./src/app/help/help.js";
+import Main from "./src/app/screens/main/main.js";
+import History from "./src/app/screens/history/history.js";
+import Settings from "./src/app/screens/settings/settings.js";
+import Help from "./src/app/screens/help/help.js";
 import { Register } from "./src/app/account/register/register.js";
 
 import { useLogin } from "./src/providers/loginProvider.js";
--- mobile/navi/src/app/screens/main/HistoricoScreen.js
+++ mobile/navi/src/app/screens/main/HistoricoScreen.js
@@ -1,10 +1,56 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import HistoricoCard from "../../components/HistoricoCard";
 
 export default function HistoricoScreen() {
+  const historico = [
+    {
+      id: "2",
+      name: "Garagem Vista Mall",
+      date: "10/11/2025",
+      duration: "1h 10min",
+      pricePaid: "R$ 8,00",
+      imageUrl:
+        "https://images.unsplash.com/photo-1506521781263-d8422e82f27a?q=80&w=1074&auto=format&fit=crop",
+    },
+
+    {
+      id: "3",
+      name: "Parking Plaza Norte",
+      date: "03/11/2025",
+      duration: "3h 45min",
+      pricePaid: "R$ 22,00",
+      imageUrl:
+        "https://images.unsplash.com/photo-1506521781225-2ddf33dba79c?q=80&w=1080&auto=format&fit=crop",
+    },
+
+    {
+      id: "4",
+      name: "Estacionamento Aeroporto Pro",
+      date: "28/10/2025",
+      duration: "5h 20min",
+      pricePaid: "R$ 40,00",
+      imageUrl:
+        "https://images.unsplash.com/photo-1517602302552-471fe67acf66?q=80&w=1074&auto=format&fit=crop",
+    },
+  ];
+
   return (
-    <View>
-      <Text style={{ fontSize: 20 }}> Página de Histórico</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Seu Histórico</Text>
+
+      {historico.map((item) => (
+        <HistoricoCard key={item.id} item={item} />
+      ))}
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { padding: 20 },
+  title: {
+    fontSize: 22,
+    fontWeight: "800",
+    marginBottom: 20,
+  },
+});
