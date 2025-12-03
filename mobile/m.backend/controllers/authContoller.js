468e16cc0ecb3f79eba625c0cad37f99a4cf7fa9
YZNYC <nycolasleandro@outlook.com>
2025-12-03 11:01:45 -0300

    FIX | AuthController corrigido estava com erro na hora do login

--- mobile/m.backend/controllers/authContoller.js
+++ mobile/m.backend/controllers/authContoller.js
@@ -37,7 +37,6 @@ const loginController = async (req, res) => {
                 email: usuario.email,
                 telefone: usuario.telefone,
                 url_foto_perfil: usuario.url_foto_perfil,
-                anoEntrada: usuario.data_criacao.substring(0, 4),
             }
         });
     } catch (error) {
